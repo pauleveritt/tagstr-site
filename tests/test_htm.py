0269ae096e7a3afa906d2210e9423368d0938f11
pauleveritt <paul.everitt@jetbrains.com>
2024-07-08 16:46:46 -0400

    Beginning to add tests.

---
+++ tests/test_htm.py
@@ -0,0 +1,47 @@
+from tagstr_site.htm import AstParser, InterpolationConcrete, html
+
+
+def test_ast_basic_parsing():
+    parser = AstParser()
+    parser.feed("<div>Hello World</div>")
+    root_node = parser.result()
+    assert "div" == root_node.tag
+    assert "Hello World" == root_node.children[0]
+
+
+def test_ast_basic_placeholder():
+    name = "World"
+    parser = AstParser()
+    parser.feed("<div>Hello ")
+    interpolation = InterpolationConcrete(lambda: name, "name", None, None)
+    parser.feed(interpolation)
+    parser.feed("</div>")
+    # Manually typing the result since IDE can't process tag functions yet
+    root_node = parser.result()
+    assert "div" == root_node.tag
+    assert ["Hello ", "x$1x"] == root_node.children
+
+
+def test_basic_tag_usage():
+    name = "World"
+    root_node = html"<div>Hello {name}</div>"
+    assert "div" == root_node.tag
+    assert ["Hello ", "World"] == root_node.children
+
+
+def test_basic_attributes():
+    name = "World"
+    level = 1
+    title = "The Greeting"
+    root_node = html'<h{level} title={title}>Hello {name}</h{level}>'
+    assert "h1" == root_node.tag
+    assert dict(title="The Greeting") == root_node.attrs
+    assert ["Hello ", "World"] == root_node.children
+
+
+def test_basic_stringifying():
+    name = "World"
+    title = "The Greeting"
+    root_node = html'<div title={title}>Hello {name}</div>'
+    result = str(root_node)
+    assert '<div title="The Greeting">Hello World</div>' == result
