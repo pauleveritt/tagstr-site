a4f592aad26255f27515138d12cfc85fff0b46bb
Dave <davepeck@gmail.com>
2024-09-23 12:07:13 -0700

    Update `Template` to match today's discussions.

--- src/tagstr_site/tstring.py
+++ src/tagstr_site/tstring.py
@@ -1,10 +1,12 @@
 from typing import (
     Any,
+    Sequence,
     Interpolation,
     Decoded,
     Protocol,
     runtime_checkable,
 )
+from dataclasses import dataclass
 
 # -----------------------------------------------------------------------
 # A simple "implementation" of proposed t-strings, using the older
@@ -14,17 +16,22 @@ from typing import (
 
 @runtime_checkable
 class Template(Protocol):
-    @property
-    def args(self) -> tuple[Interpolation | str, ...]: ...
+    # CONSIDER: in this example, `source` is a tuple of `str(Decoded)` for all
+    # the `Decoded`s, while `raw` is a tuple of `Decoded.raw`. Do we want both?
+    # Can both be constructed at compile time?
+    source: tuple[str, ...]
+    raw: tuple[str, ...]
+
+    values: tuple[Any, ...]
+    exprs: tuple[str, ...]
+    convs: tuple[str | None, ...]
+    format_specs: tuple[str | None, ...]
 
     @property
-    def source(self) -> tuple[str, ...]: ...
-
-    @property
-    def raw(self) -> str: ...
+    def args(self) -> Sequence[Interpolation | Decoded]: ...
 
 
-class _ValueInterpolation(Interpolation):
+class _InterpolationLiteral(Interpolation):
     def __init__(
         self, value: Any, expr: str, conv: str | None, format_spec: str | None
     ):
@@ -37,33 +44,68 @@ class _ValueInterpolation(Interpolation):
         return self.value
 
 
-
-class _TemplateConcrete(Template):
-    _args: tuple[Interpolation | str, ...]
-    _source: tuple[str, ...]
+class _DecodedLiteral(str):
     _raw: str
 
-    def __init__(self, args: tuple[Interpolation | str, ...], source: tuple[str, ...], raw: str):
-        self._args = args
-        self._source = source
+    def __new__(cls, value: str, raw: str):
+        self = super().__new__(cls, value)
         self._raw = raw
-
-    @property
-    def args(self) -> tuple[Interpolation | str, ...]:
-        return self._args
-
-    @property
-    def source(self) -> tuple[str, ...]:
-        return self._source
+        return self
 
     @property
     def raw(self) -> str:
         return self._raw
 
-    # CONSIDER what, if anything, should __str__() return?
+
+@dataclass(frozen=True)
+class _TemplateLiteral(Template):
+    source: tuple[str, ...]
+    raw: tuple[str, ...]
+    values: tuple[Any, ...]
+    exprs: tuple[str, ...]
+    convs: tuple[str | None, ...]
+    format_specs: tuple[str | None, ...]
+
+    def __post_init__(self):
+        if not self.source:
+            raise ValueError("source must not be empty")
+        if len(self.source) != len(self.raw):
+            raise ValueError("source and raw must have the same length")
+        if len(self.values) != len(self.exprs) or len(self.values) != len(self.convs) or len(self.values) != len(self.format_specs):
+            raise ValueError("values, exprs, convs, and format_specs must have the same length")
+        if len(self.values) + 1 != len(self.source):
+            raise ValueError("values must be one less than the length of source")
+
+    @property
+    def args(self) -> Sequence[Interpolation | Decoded]:
+        return tuple(
+            (
+                _DecodedLiteral(self.source[i // 2], self.raw[i // 2])
+                if i % 2 == 0
+                else _InterpolationLiteral(
+                    self.values[i // 2],
+                    self.exprs[i // 2],
+                    self.convs[i // 2],
+                    self.format_specs[i // 2],
+                )
+            )
+            for i in range(len(self.values) + len(self.source))
+        )
+
+
 
 
 def t(*args: Interpolation | str) -> Template:
+    """
+    Implement the proposed PEP 750 template string behavior, using the
+    older cpython implementation of tagged strings.
+
+    This is hilariously inefficient and exists only to demonstrate the
+    expected behavior of a full implementation of PEP 750.
+
+    See test_tstring.py for examples of the expected behavior.
+    """
+
     # Our current cpython implementation of old-school tagged strings does
     # NOT provide even/odd behavior for `Decoded` and `Interpolation`.
     # So we effectively create it here.
@@ -81,17 +123,17 @@ def t(*args: Interpolation | str) -> Template:
     # some_tag"" -> ()  # empty tuple
     # some_tag"{42}" -> (Interpolation(42))  # no blank strings
     # some_tag"{42}{99}" -> (Interpolation(42), Interpolation(99))  # no blank strings
+    #
+    # see `test_tstring.py` for examples.
 
-    eo_args: list[Interpolation | str] = []
+    eo_args: list[Interpolation | Decoded] = []
     last_was_str: bool = False
 
     for arg in args:
         if isinstance(arg, Interpolation):
             if not last_was_str:
-                # TODO ideally we'd construct something that conforms to `Decoded`
-                # here. But AFAIK we *can't* with the current cpython implementation.
-                eo_args.append("")
-            value_interpolation = _ValueInterpolation(arg.getvalue(), arg.expr, arg.conv, arg.format_spec)  # type: ignore
+                eo_args.append(_DecodedLiteral("", ""))
+            value_interpolation = _InterpolationLiteral(arg.getvalue(), arg.expr, arg.conv, arg.format_spec)  # type: ignore
             eo_args.append(value_interpolation)
             last_was_str = False
         else:
@@ -99,35 +141,16 @@ def t(*args: Interpolation | str) -> Template:
             last_was_str = True
 
     if not last_was_str:
-        eo_args.append("")
+        eo_args.append(_DecodedLiteral("", ""))
 
-    # Make a tuple of the odd elements, which is good to memoize off of
-    source = tuple(eo_args[::2])
+    assert len(eo_args) >= 1
+    assert len(eo_args) % 2 == 1
 
-    # Cobble together something like a `raw` string that we might include in
-    # a full implementation of PEP 750. This is a hack and it does not fully
-    # work. It also does not support `conv` or `format_spec`.
-    raw = "".join(
-        f"{{{arg.expr}}}" if isinstance(arg, Interpolation) else arg for arg in eo_args  # type: ignore
-    )
-    return _TemplateConcrete(tuple(eo_args), source, raw)
+    source = tuple(str(arg) for arg in eo_args[::2])
+    raw = tuple(arg.raw for arg in eo_args[::2])
+    values = tuple(arg.value for arg in eo_args[1::2])
+    exprs = tuple(arg.expr for arg in eo_args[1::2])
+    convs = tuple(arg.conv for arg in eo_args[1::2])
+    format_specs = tuple(arg.format_spec for arg in eo_args[1::2])
 
-
-# >>> from tagstr_site.tstring import t
-# >>> x = t"hello {42}"
-# >>> x.source
-# (DecodedConcrete('hello '), '')
-# >>> x.raw
-# 'hello {42}'
-# >>> x.args[0]
-# DecodedConcrete('hello ')
-# >>> x.args[1]
-# <tagstr_site.tstring._ValueInterpolation object at 0xffffb72541a0>
-# >>> x.args[1].value
-# 42
-# >>> x.args[2]
-# ''
-
-
-
-
+    return _TemplateLiteral(source, raw, values, exprs, convs, format_specs)
